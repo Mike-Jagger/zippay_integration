a977413c3b33e6eba06ee367ea136b606a6c64eb
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-31 22:10:54 -0600

    adding initial project implementation files

---
+++ util.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const NodeRSA = require('node-rsa');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
