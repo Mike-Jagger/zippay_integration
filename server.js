f35e9689e13389d94d15902d93c72f78ff219a1e
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-01 11:17:21 -0600

    Added middleware to handle merchant balance inquiry

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const { encryptData, decryptData } = require('./util');
 const paymentPayout = require('./routes/exchangeOrder');
 const paymentPayin = require('./routes/rechargeOrder');
+const merchantQuery = require('./routes/merchantQuery');
 
 
 const app = express();
@@ -46,31 +47,7 @@ app.post('/payment/rechargeOrderquery', async (req, res) => {
 });
 
 // Balance Inquiry Endpoint
-app.post('/payment/merchantQuery', async (req, res) => {
-    const { merchantId, nonce, timestamp } = req.body;
-    
-    const signData = `${merchantId}${nonce}${timestamp}`;
-    const sign = encryptData(signData);
-
-    const payload = {
-        merchantId,
-        nonce,
-        timestamp,
-        sign
-    };
-
-    try {
-        const response = await axios.post('http://52.74.165.63:8040/api/payment/merchantQuery', payload, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+app.use('/payment/merchantQuery', merchantQuery);
 
 // Payment Endpoint
 app.use('/payment/payout', paymentPayout);
