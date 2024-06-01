054855a3f4bacdf8037682d961ebc39b6c4d36db
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-01 15:36:27 -0600

    Added method to clean up decrypted data

--- zippayAPI/server.js
+++ zippayAPI/server.js
@@ -3,29 +3,26 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 const axios = require('axios');
+const { encryptData, decryptData, constructSignData, cleanUpDecryptedData } = require('./utils');
 
 app.use(bodyParser.json());
 
 app.post('/payment/payout', (req, res) => {
     const payLoad = req.body;
-    console.log(payLoad)
     
-    // Simulate processing the request
-    setTimeout(() => {
-        // Simulate callback to the backend
-        axios.post('http://localhost:5000/payment/payout/callback', {
-            code: 1,
-            msg: 'SUCCESS',
-            merchantId: payLoad.merchantOrderId,
-            platformOrderId: `P${Date.now()}`,
-            sign: 'mocked-signature',
-            utr: 'mocked-utr'
-        }).then(() => {
-            console.log('Callback sent successfully');
-        }).catch((err) => {
-            console.error('Error sending callback:', err.message);
-        });
-    }, 3000); // Simulate delay
+    // Start decryption
+    const signData = payLoad.merchantId + payLoad.merchantOrderId + payLoad.amount + payLoad.nonce + payLoad.timestamp;
+
+    const isValidSign = cleanUpDecryptedData(decryptData(payLoad.sign)) == signData;
+
+    if (isValidSign) {
+        console.log("Yay!")
+
+    } else {
+        res.status(400).send('invalid signature');
+    }
+
+    
 
     res.json({
         merchantId: 1,
@@ -33,6 +30,25 @@ app.post('/payment/payout', (req, res) => {
         merchanOrderId: payLoad.merchanOrderId,
         platformOrderId: `P${Date.now()}`
     });
+
+    // Simulating a successful payout
+    
+    // Simulate processing the request
+    // setTimeout(() => {
+    //     // Simulate callback to the backend
+    //     axios.post('http://localhost:5000/payment/payout/callback', {
+    //         code: 1,
+    //         msg: 'SUCCESS',
+    //         merchantId: payLoad.merchantOrderId,
+    //         platformOrderId: `P${Date.now()}`,
+    //         sign: 'mocked-signature',
+    //         utr: 'mocked-utr'
+    //     }).then(() => {
+    //         console.log('Callback sent successfully');
+    //     }).catch((err) => {
+    //         console.error('Error sending callback:', err.message);
+    //     });
+    // }, 3000); // Simulate delay
 });
 
 app.listen(PORT, () => {
