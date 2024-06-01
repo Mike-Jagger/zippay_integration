72699cb335e52eb3430b70c59fdf1a837f420287
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-01 14:45:34 -0600

    Adding utils.js

---
+++ zippayAPI/utils.js
@@ -0,0 +1,27 @@
+const NodeRSA = require('node-rsa');
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('./KEYS/PRIVATE.pem', 'utf8');
+const publicKey = fs.readFileSync('./KEYS/PUBLIC.pem', 'utf8');
+
+const privateKeyObject = new NodeRSA(privateKey, 'pkcs8-private-pem');
+const publicKeyObject = new NodeRSA(publicKey, 'pkcs8-public-pem');
+
+const encryptData = (data) => {
+    return privateKeyObject.encryptPrivate(Buffer.from(JSON.stringify(data)), 'base64');
+};
+
+const decryptData = (data) => {
+    return publicKeyObject.decryptPublic(Buffer.from(data, 'base64'), 'utf8');
+};
+
+const constructSignData = (data) => {
+    const sortedKeys = Object.keys(data).sort();
+    return sortedKeys.map(key => data[key]).join('');
+};
+
+module.exports = {
+    encryptData,
+    decryptData,
+    constructSignData
+};
